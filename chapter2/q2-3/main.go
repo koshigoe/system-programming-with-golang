de2031396bc19fbdab7bd54a75e7ff4c581b77ff
koshigoe <koshigoeb@gmail.com>
2017-11-19 19:26:20 +0900

    Q2.3 gzip された JSON 出力をしながら、標準出力にログを出力 (p. 33)

---
+++ chapter2/q2-3/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"compress/gzip"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Encoding", "gzip")
+	w.Header().Set("Content-Type", "application/json")
+
+	source := map[string]string{
+		"Hello": "World",
+	}
+	g := gzip.NewWriter(w)
+	out := io.MultiWriter(g, os.Stdout)
+	encoder := json.NewEncoder(out)
+	encoder.SetIndent("", "    ")
+	if err := encoder.Encode(source); err != nil {
+		log.Print(err)
+	}
+	if err := g.Close(); err != nil {
+		log.Print(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
